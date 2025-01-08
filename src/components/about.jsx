64c55e4fbda0e43a30d720c06a6f0b743cf29e22
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-08 07:55:26 +0530

    create About Component page📄

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div
+      className=" flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+      id="about"
+    >
+      <h1>
+        About <span>Our Brand</span>
+        <p>Passionate About Properties, dedicated to Your Vision </p>
+      </h1>
+    </div>
+  );
+}
