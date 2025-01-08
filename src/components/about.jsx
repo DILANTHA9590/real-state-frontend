473bf2ba761ece8a24a06b48f74982f3850aff4c
DILANTHA9590 <dilanthanayanajith@gmail.com>
2025-01-08 11:46:02 +0530

    📄 Add: About section with brand details and statistics

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,64 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 export default function About() {
   return (
     <div
-      className=" flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
-      id="about"
+      className=" min-h-screen flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden"
+      id="bout"
     >
-      <h1>
-        About <span>Our Brand</span>
-        <p>Passionate About Properties, dedicated to Your Vision </p>
+      <h1 className="text-xl sm:text-4xl font-bold mb-2">
+        About{" "}
+        <span className="underline underline-offset-8 decoration-1 under font-light">
+          Our Brand
+        </span>
       </h1>
+      <p className="text-gray-500 max-w-80   text-center mb-8">
+        Passionate About Properties, dedicated to Your Vision
+      </p>
+      <div
+        className="flex 
+          flex-col  md:flex-row items-center md:items-start md:gap-20"
+      >
+        <img
+          src={assets.brand_img}
+          alt=""
+          className="w-full sm:w-1/2 max-w-lg"
+        />
+
+        <div className="flex flex-col items-start mt-10 text-gray-600">
+          <div className="grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28">
+            <div>
+              <p className="text-4xl font-medium text-gray-800">10+</p>
+              <p>Years of Excellence</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">12+</p>
+              <p>Project Completed</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">20+</p>
+              <p>Mn. Sq. Ft. Delivered</p>
+            </div>
+
+            <div>
+              <p className="text-4xl font-medium text-gray-800">25+</p>
+              <p>Ongoing Project</p>
+            </div>
+          </div>
+          <p className="my-10 max-w-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus illum
+            dolorem voluptates minima molestiae aliquid temporibus accusantium
+            cupiditate. Praesentium enim iure quae quaerat voluptate sequi a sit
+            libero laborum dolorem!
+          </p>
+          <button className="bg-blue-600 text-white py-2 px-4 rounded">
+            Learn more
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
